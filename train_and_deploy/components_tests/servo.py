3aba52733256e2f5a7a33bf964f3f0dc226b61ac
linzhanguca <lzhang12@uca.edu>
2023-10-11 16:05:28 -0500

    update servo test

--- train_and_deploy/components_tests/servo.py
+++ train_and_deploy/components_tests/servo.py
@@ -1,16 +1,14 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(17, min_angle=-90, max_angle=90)
+servo = AngularServo(17, min_angle=0, max_angle=180)
 
-while True:
-    servo.angle = -90
-    sleep(2)
-    servo.angle = -45
-    sleep(2)
-    servo.angle = 0
-    sleep(2)
-    servo.angle = 45
-    sleep(2)
-    servo.angle = 90
-    sleep(2)
+for a in range(181):
+    if not a%45:
+        servo.angle = a
+        print(f"angle: {a}")
+        sleep(2)
+
+servo.angle = 90
+print("CENTER")
+sleep(2)
