328d7fa16990f571b74d3cbf98c1bfc6d132f20b
linzhanguca <lzhang12@uca.edu>
2023-10-12 16:03:11 -0500

    update customized dataset class

--- train_and_deploy/train.py
+++ train_and_deploy/train.py
@@ -1,8 +1,4 @@
-
-# Train an autopilot for autonomous ground vehicle using
-# convolutional neural network and labeled images. 
-
-
+import sys
 import os
 import numpy as np
 import pandas as pd
@@ -16,39 +12,11 @@ import cnn_network
 import cv2 as cv
 
 
-# Designate processing unit for CNN training
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using {DEVICE} device")
-
-
-class CustomImageDataset(Dataset): 
-
-    # Create a dataset from our collected data
-
-    def __init__(self, annotations_file, img_dir, transform=transforms.ToTensor()):
-        self.img_labels = pd.read_csv(annotations_file)
-        self.img_dir = img_dir
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.img_labels)
-
-    def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = cv.imread(img_path, cv.IMREAD_COLOR)
-        steering = self.img_labels.iloc[idx, 1].astype(np.float32)
-        throttle = self.img_labels.iloc[idx, 2].astype(np.float32)
-        if self.transform:
-            image = self.transform(image)
-        return image.float(), steering, throttle
-
 
 
 def train(dataloader, model, loss_fn, optimizer):
     
-    # Define Training Function
-    
-    size = len(dataloader.dataset)
+    num_samples = len(dataloader.dataset)
     model.train()
     epoch_loss = 0.0
 
@@ -94,76 +62,98 @@ def test(dataloader, model, loss_fn):
 
 
 
+class BearCartDataset(Dataset): 
+    """
+    Customize dataset class
+    """
 
-if __name__ == '__main__':
+    def __init__(self, annotation_path, image_dir, transform=transforms.ToTensor()):
+        self.img_notes = pd.read_csv(annotation_path, header=None)
+        self.img_dir = image_dir
+        self.transform = transform
 
-    # Create a dataset
-    annotations_file = "/home/robotics-j/Autobots/train_and_deploy/data/2023_10_10_15_44/labels.csv"  # the name of the csv file
-    img_dir = "/home/robotics-j/Autobots/train_and_deploy/data/2023_10_10_15_44/images"  # the name of the folder with all the images in it
-    collected_data = CustomImageDataset(annotations_file, img_dir)
-    print("data length: ", len(collected_data))
+    def __len__(self):
+        return len(self.img_notes)
 
-    # Define the size for train and test data
-    train_data_len = len(collected_data)
-    train_data_size = round(train_data_len*0.9)
-    test_data_size = train_data_len - train_data_size 
-    print("len and train and test: ", train_data_len, " ", train_data_size, " ", test_data_size)
-
-    # Load the datset (split into train and test)
-    train_data, test_data = random_split(collected_data, [train_data_size, test_data_size])
-    train_dataloader = DataLoader(train_data, batch_size=125)
-    test_dataloader = DataLoader(test_data, batch_size=125)
-
-
-    # Initialize the model
-    # Models that train well:
-    #     lr = 0.001, epochs = 10
-    #     lr = 0.0001, epochs = 15 (epochs = 20 might also work)
-    model = cnn_network.DonkeyNet().to(DEVICE) # choose the architecture class from cnn_network.py
-    loss_fn = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr= 0.001)
-    epochs = 15
-
-    # Optimize the model
-    train_loss = []
-    test_loss = []
-    for t in range(epochs):
-        print(f"Epoch {t+1}\n-------------------------------")
-        training_loss = train(train_dataloader, model, loss_fn, optimizer)
-        testing_loss = test(test_dataloader, model, loss_fn)
-        print("average training loss: ", training_loss)
-        print("average testing loss: ", testing_loss)
-        # save values
-        train_loss.append(training_loss)
-        test_loss.append(testing_loss)   
-
-    print(f"Optimize Done!")
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_notes.iloc[idx, 0])
+        img_arr = cv.imread(img_path, cv.IMREAD_COLOR)
+        steering = self.img_notes.iloc[idx, 1].astype(np.float32)
+        throttle = self.img_notes.iloc[idx, 2].astype(np.float32)
+        image_tensor = transforms.ToTensor(img_arr).float
+        return image_tensor, steering, throttle
 
 
-    #print("final test lost: ", test_loss[-1])
-    len_train_loss = len(train_loss)
-    len_test_loss = len(test_loss)
-    print("Train loss length: ", len_train_loss)
-    print("Test loss length: ", len_test_loss)
+# SETUP
+# Designate processing unit for CNN training
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {DEVICE} device")
+# Create a dataloaders
+data_dir = os.path.join(sys.path[0], "data", "2023_10_12_14_52")
+annotation_path = os.path.join(data_dir, "annotations.csv")
+image_dir = os.path.join(data_dir, "images/")
+dataset = BearCartDataset(annotation_path, image_dir)
+print("data length: ", len(dataset))
 
+# # Define the size for train and test data
+# train_data_len = len(collected_data)
+# train_data_size = round(train_data_len*0.9)
+# test_data_size = train_data_len - train_data_size 
+# print("len and train and test: ", train_data_len, " ", train_data_size, " ", test_data_size)
+#
+# # Load the datset (split into train and test)
+# train_data, test_data = random_split(collected_data, [train_data_size, test_data_size])
+# train_dataloader = DataLoader(train_data, batch_size=125)
+# test_dataloader = DataLoader(test_data, batch_size=125)
+#
+#
+# # Initialize the model
+# # Models that train well:
+# #     lr = 0.001, epochs = 10
+# #     lr = 0.0001, epochs = 15 (epochs = 20 might also work)
+# model = cnn_network.DonkeyNet().to(DEVICE) # choose the architecture class from cnn_network.py
+# loss_fn = nn.MSELoss()
+# optimizer = torch.optim.Adam(model.parameters(), lr= 0.001)
+# epochs = 15
+#
+# # Optimize the model
+# train_loss = []
+# test_loss = []
+# for t in range(epochs):
+#     print(f"Epoch {t+1}\n-------------------------------")
+#     training_loss = train(train_dataloader, model, loss_fn, optimizer)
+#     testing_loss = test(test_dataloader, model, loss_fn)
+#     print("average training loss: ", training_loss)
+#     print("average testing loss: ", testing_loss)
+#     # save values
+#     train_loss.append(training_loss)
+#     test_loss.append(testing_loss)   
+#
+# print(f"Optimize Done!")
+#
+#
+# #print("final test lost: ", test_loss[-1])
+# len_train_loss = len(train_loss)
+# len_test_loss = len(test_loss)
+# print("Train loss length: ", len_train_loss)
+# print("Test loss length: ", len_test_loss)
+#
+#
+# # create array for x values for plotting train
+# epochs_array = list(range(epochs))
+#
+# # Graph the test and train data
+# fig = plt.figure()
+# axs = fig.add_subplot(1,1,1)
+# plt.plot(epochs_array, train_loss, color='b', label="Training Loss")
+# plt.plot(epochs_array, test_loss, '--', color='orange', label='Testing Loss')
+# axs.set_ylabel('Loss')
+# axs.set_xlabel('Training Epoch')
+# axs.set_title('DonkeyNet 15 Epochs lr=1e-3')
+# axs.legend()
+# # fig.savefig('/home/robotics-j/Autobots/train_and_deploy/data/2023_10_10_15_44/DonkeyNet_15_epochs_lr_1e_3.png')
+# fig.savefig(os.path.join(os.path.dirname(img_dir),'DonkeyNet_15_epochs_lr_1e_3.png'))
+#
+# # Save the model
+# torch.save(model.state_dict(), os.path.join(os.path.dirname(img_dir),"DonkeyNet_15_epochs_lr_1e_3.pth"))
 
-    # create array for x values for plotting train
-    epochs_array = list(range(epochs))
-
-    # Graph the test and train data
-    fig = plt.figure()
-    axs = fig.add_subplot(1,1,1)
-    plt.plot(epochs_array, train_loss, color='b', label="Training Loss")
-    plt.plot(epochs_array, test_loss, '--', color='orange', label='Testing Loss')
-    axs.set_ylabel('Loss')
-    axs.set_xlabel('Training Epoch')
-    axs.set_title('DonkeyNet 15 Epochs lr=1e-3')
-    axs.legend()
-    # fig.savefig('/home/robotics-j/Autobots/train_and_deploy/data/2023_10_10_15_44/DonkeyNet_15_epochs_lr_1e_3.png')
-    fig.savefig(os.path.join(os.path.dirname(img_dir),'DonkeyNet_15_epochs_lr_1e_3.png'))
-
-    # Save the model
-    torch.save(model.state_dict(), os.path.join(os.path.dirname(img_dir),"DonkeyNet_15_epochs_lr_1e_3.pth"))
-
-
-    
